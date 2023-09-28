5259719acbbeb9861186609b8107b6b9524c70db
Kirill Glinskikh <glinskix24@bk.ru>
2023-09-28 21:38:49 +0300

    db

---
+++ cmd/api/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"dev/projects/training_server_bd/internal/app/api"
+	"flag"
+	"log"
+
+	"github.com/BurntSushi/toml"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "path", "configs/api.toml", "path to config file in .toml format")
+}
+
+func main() {
+	flag.Parse()
+	log.Println("It works")
+	config := api.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Println(err)
+	}
+
+	server := api.New(config)
+
+	if err := server.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module dev/projects/training_server_bd
+
+go 1.21.1
+
+require github.com/BurntSushi/toml v1.3.2 // indirect
